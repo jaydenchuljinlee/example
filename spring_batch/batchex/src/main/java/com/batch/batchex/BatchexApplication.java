4cb6f61bc8c2b83be4c8013e73d10d9e30ba3976
cheojlin <cheoljin1234@naver.com>
2022-10-31 10:25:47 +0900

    added spring batch server

---
+++ spring_batch/batchex/src/main/java/com/batch/batchex/BatchexApplication.java
@@ -0,0 +1,13 @@
+package com.batch.batchex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchexApplication.class, args);
+	}
+
+}
---
+++ spring_batch/batchex/src/test/java/com/batch/batchex/BatchexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.batchex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
