4e8e492714d4857d0347b05cc62d93ce76b26ffd
cheojlin <cheoljin1234@naver.com>
2022-11-27 11:28:41 +0900

    adaptor pattern

---
+++ headfirst_designpattern/designpattern/src/test/java/com/headfirst/designpattern/adaptor/iteration/EnumerationIteratorTest.java
@@ -0,0 +1,11 @@
+package com.headfirst.designpattern.adaptor.iteration;
+
+import org.junit.jupiter.api.Test;
+
+public class EnumerationIteratorTest {
+    @Test
+    public void Enumeration_To_Iterator() {}
+
+    @Test
+    public void Iterator_To_Enumeration() {}
+}
