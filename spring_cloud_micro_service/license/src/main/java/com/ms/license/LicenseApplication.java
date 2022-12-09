1b39994b7328abc44613d0b0f277511174c8b17a
cheojlin <cheoljin1234@naver.com>
2022-12-09 18:20:32 +0900

    modified the codes related eureka

--- spring_cloud_micro_service/license/src/main/java/com/ms/license/LicenseApplication.java
+++ spring_cloud_micro_service/license/src/main/java/com/ms/license/LicenseApplication.java
@@ -3,7 +3,11 @@ package com.ms.license;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableEurekaClient
 @RefreshScope
 @SpringBootApplication
 public class LicenseApplication {
