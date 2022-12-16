30798d3fe31f94986440cf23941b8d9c22c9d1fd
cheojlin <cheoljin1234@naver.com>
2022-12-16 17:38:51 +0900

    added gateway

--- spring_cloud_micro_service/gateway/src/main/java/com/ms/gateway/GatewayApplication.java
+++ spring_cloud_micro_service/gateway/src/main/java/com/ms/gateway/GatewayApplication.java
@@ -2,9 +2,8 @@ package com.ms.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+
 @SpringBootApplication
 public class GatewayApplication {
 
