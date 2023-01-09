01c72a1ff470171f9b0401a6a4992e211547dd43
cheojlin <cheoljin1234@naver.com>
2023-01-09 18:04:41 +0900

    added spring deployment with Docker

---
+++ docker/demo/src/main/java/com/example/demo/controller/CommonController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class CommonController {
+
+    @ResponseBody
+    @GetMapping
+    public String example() {
+        return "hello";
+    }
+}
